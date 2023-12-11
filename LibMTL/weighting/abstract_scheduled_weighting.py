597a6abaa9f6aa53480bc8bbd56dcdf0fa4b7b16
Patrick Schmidt <58435315+the-patpat@users.noreply.github.com>
2023-12-11 11:01:19 +0100

    abstract delayed weighting

---
+++ LibMTL/weighting/abstract_scheduled_weighting.py
@@ -0,0 +1,19 @@
+import numpy as np
+import torch
+from LibMTL.weighting import AbsWeighting
+
+class AbsScheduledWeighting(AbsWeighting):
+    def __init__(self):
+        super(AbsScheduledWeighting, self).__init__()
+    
+    def _backward(self, losses, **kwargs):
+        pass
+    
+    def backward(self, losses, **kwargs):
+        if self.epoch < 10:
+            # Do EW in the beginning
+            loss = torch.mul(losses, torch.ones_like(losses).to(self.device)).sum()
+            loss.backward()
+            return np.ones(self.task_num)
+        else:
+            return self._backward(losses, **kwargs)
